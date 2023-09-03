ba1774b3ef8211099a9ff4c8244d859270fc65b7
Tanyaj31 <142903725+Tanyaj31@users.noreply.github.com>
2023-09-04 00:47:24 +0300

    Exercise2.4

--- Module2/Ex2.4.py
+++ Module2/Ex2.4.py
@@ -1,9 +1,9 @@
-number1 = int(input("Enter the first integer: "))
-number2 = int(input("Enter the second integer: "))
-number3 = int(input("Enter the third integer: "))
+number1 = int(input("Enter the first integer number: "))
+number2 = int(input("Enter the second integer number: "))
+number3 = int(input("Enter the third integer number: "))
 sum_numbers = number1 + number2 + number3
 product = number1 * number2 * number3
 average = sum_numbers / 3
-print("Sum:", sum_numbers)
-print("Product:", product)
-print("Average:", average)
+print("The sum of the numbers is:", sum_numbers)
+print("The product of the numbers is:", product)
+print("The average of the numbers is:", average)
