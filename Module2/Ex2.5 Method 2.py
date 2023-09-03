37e1d9b1a7dca450f689244074b11b588eb2f14d
Tanyaj31 <142903725+Tanyaj31@users.noreply.github.com>
2023-09-04 00:55:09 +0300

    Exercise2.5

--- Module2/Ex2.5 Method 2.py
+++ Module2/Ex2.5 Method 2.py
@@ -1,9 +1,9 @@
 Pounds_Per_Talent = 20
 Lots_Per_Pound = 32
 Grams_Per_Lot = 13.3
-Talents = float(input("\nEnter a mass in talents:\n"))
-Pounds = float(input("\nEnter a mass in pounds:\n"))
-Lots = float(input("\nEnter a mass in lots:\n"))
+Talents = float(input("\nEnter a mass in talents (leiviskä):\n"))
+Pounds = float(input("\nEnter a mass in pounds (naula):\n"))
+Lots = float(input("\nEnter a mass in lots (luoti):\n"))
 Total_lots = (Talents * Pounds_Per_Talent + Pounds) * Lots_Per_Pound + Lots
 grams = Total_lots * Grams_Per_Lot
 kilograms = int(grams // 1000)
