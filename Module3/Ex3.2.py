f49a53268a08289f86ca3e88a49059e9e0cddc38
Tanyaj31 <142903725+Tanyaj31@users.noreply.github.com>
2023-09-04 01:09:06 +0300

    Exercise3.2

--- Module3/Ex3.2.py
+++ Module3/Ex3.2.py
@@ -1,14 +1,11 @@
-cabin_class = input("Enter the cabin class (LUX, A, B, C): ")
-
+cabin_class = input("Enter the cabin class of a cruise ship (LUX, A, B, C): ")
 if cabin_class == "LUX":
-    description = "upper-deck cabin with a balcony."
+    print("It is an upper-deck cabin with a balcony.")
 elif cabin_class == "A":
-    description = "above the car deck, equipped with a window."
+    print("It is above the car deck, equipped with a window.")
 elif cabin_class == "B":
-    description = "windowless cabin above the car deck."
+    print("It is a windowless cabin above the car deck.")
 elif cabin_class == "C":
-    description = "windowless cabin below the car deck."
+    print("It is a windowless cabin below the car deck.")
 else:
-    description = "Invalid cabin class"
-
-print(description)
+    print("Invalid cabin class")
