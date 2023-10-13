4f1b5f2a54b84b5d3530a82e1d7e659509bcce25
Tanyaj31 <142903725+Tanyaj31@users.noreply.github.com>
2023-10-13 13:15:26 +0300

    Delete Test directory

--- Test/Practice 2.py
+++
@@ -1,9 +0,0 @@
-while True:
-    number= int(input("Please enter an integer number: "))
-    if number <= 0:
-        print("Stop")
-        break
-    factorial = number
-    for no in range(1, number):
-        factorial *= no
-        print(f"The factorial of the number {number} is {factorial}")
