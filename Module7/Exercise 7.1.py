d7cccefeca07eb91d75b1459f0553b65d6bd9580
Tanyaj31 <142903725+Tanyaj31@users.noreply.github.com>
2023-10-21 01:13:17 +0300

    Exercise 7.1

---
+++ Module7/Exercise 7.1.py
@@ -0,0 +1,15 @@
+seasons = ('Winter', 'Spring', 'Summer', 'Autumn')
+month = int(input("Enter the number of any month ranging from 1 to 12: "))
+
+if 1 <= month <= 3:
+    season = seasons[0]
+elif 4 <= month <= 6:
+    season = seasons[1]
+elif 7 <= month <= 9:
+    season = seasons[2]
+elif 10 <= month <= 12:
+    season = seasons[3]
+else:
+    season= print("You have entered an invalid number of a month.")
+
+print(f"The season for month {month} is {season}.")
