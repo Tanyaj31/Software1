bb7f067031161fad6409234be041e5ca22d23727
Tanyaj31 <142903725+Tanyaj31@users.noreply.github.com>
2023-10-20 18:02:00 +0300

    Exercise 6.4

---
+++ Module6/Exercise 6.4.py
@@ -0,0 +1,8 @@
+def sum_num(numbers):
+    total = sum(numbers)
+    return total
+
+list = [2, 3, 4, 10, 11]
+
+answer = sum_num(list)
+print(f"The sum of all of the integers in the list is: {answer}")
