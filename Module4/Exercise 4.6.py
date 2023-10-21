9b938252551e60141301cdaeb41da079c2199f1a
Tanyaj31 <142903725+Tanyaj31@users.noreply.github.com>
2023-10-21 14:57:42 +0300

    Exercise 4.6

--- Module4/Exercise 4.6.py
+++ Module4/Exercise 4.6.py
@@ -1,15 +1,15 @@
 import random
-dots = int(input("Enter the total number of dots: "))
+
+dots = int(input("Type in the total number of dots: "))
 cp = 0
-sp = 0
-d1 = 0
-while d1 < dots:
-    randx = random.uniform(-1,1)
-    randy = random.uniform(-1,1)
-    distant = randx*2 + randy*2
-    if distant <=1:
+
+for _ in range(dots):
+    randx = random.uniform(-1, 1)
+    randy = random.uniform(-1, 1)
+    distant = randx ** 2 + randy ** 2
+    if distant <= 1:
         cp = cp + 1
-    d1 = d1 + 1
-    sp = sp + 1
-    pi = 4*cp/sp
-print("The estimated value of pi is: ", pi)
+
+pi = 4 * cp / dots
+
+print("The estimated value of pi (π) is:", pi)
