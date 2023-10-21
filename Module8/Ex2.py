1e8aec1abb9bfdc06740ac1c903b074ae772ceed
Tanyaj31 <142903725+Tanyaj31@users.noreply.github.com>
2023-10-21 13:57:54 +0300

    Exercise 8.2

--- Module8/Ex2.py
+++ Module8/Ex2.py
@@ -1,28 +1,27 @@
 import mysql.connector
 
-conn = mysql.connector.connect( host="your_mysql_host", user="your_mysql_user",
-
-password="your_mysql_password",
-
-database="airports"
-
+conn = mysql.connector.connect(
+    host="127.0.0.1",
+    user="Tanya",
+    password="Rst100",
+    database="flight_game"
 )
 
 cursor = conn.cursor()
 
-country_code = input("Enter the country code (e.g., FI for Finland): ").strip().upper()
+country_code = input("Type in the country code (for example, FI for Finland): ").strip().upper()
 
 cursor.execute("SELECT ident, name, type FROM airport WHERE LEFT(ident, 2) = %s ORDER BY type", (country_code,))
 
 results = cursor.fetchall()
 
 if results:
- print(f"Airports in {country_code}:")
+ print(f"The airports in {country_code}:")
 
 for ident, name, airport_type in results: print(f"{name} ({airport_type} airport) [{ident}]")
 else:
 
- print(f"No airports found in {country_code}.")
+ print(f"Sorry, but no airports are found in the country with the country code {country_code}.")
  cursor.close()
 
 conn.close()
